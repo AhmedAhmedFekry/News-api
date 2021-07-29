467f477d2f50a4a1957cc5fb7a3c8417bb63b81b
AhmedAhmedFekry <ahmedahmedfekry11305654@gmail.com>
2021-07-29 12:26:38 +0200

    done

--- api/views.py
+++ api/views.py
@@ -4,7 +4,24 @@ from django.http.response import JsonResponse
 # Create your views here.
 api_key='584f5f7739d24380af0c8ac6ac9f4c8f'
 def home(request):
-    url=f'https://newsapi.org/v2/top-headlines/sources?category=business&apiKey={api_key}'
-    response = requests.get(url)
-    response= response.json()
-    return JsonResponse({'data':response})
+    country = request.GET.get('country')
+    category = request.GET.get('category')
+
+    if country:
+        url = f'https://newsapi.org/v2/top-headlines?country={country}&apiKey={api_key}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+    else:
+        url = f'https://newsapi.org/v2/top-headlines?category={category}&apiKey={api_key}'
+        response = requests.get(url)
+        data = response.json()
+        articles = data['articles']
+
+
+
+    context = {
+        'articles' : articles
+    }
+
+    return render(request, 'home.html', context)
